581dab68a4a6075c8899a6c6c25a1867759a3c85
James Mead <james@floehopper.org>
2015-06-04 10:28:47 +0100

    Update worldwide location organisation fixtures
    Extended the `update-worldwide-locations` script to download the organisations
    JSON for each country from the production server.
    
    It looks as if a bunch of the data was out-of-date and we were missing data
    for a bunch of countries.

--- script/update-worldwide-locations.rb
+++ script/update-worldwide-locations.rb
@@ -1,6 +1,18 @@
+require "open-uri"
+
 world_locations_fixture_path = Rails.root.join("test/fixtures/worldwide_locations.yml")
 
+locations = []
 File.open(world_locations_fixture_path, "w") do |file|
   locations = WorldLocation.all.map(&:slug)
   file.puts locations.to_yaml
+end
+
+locations.each do |slug|
+  puts slug
+  json = open("https://www.gov.uk/api/world-locations/#{slug}/organisations.json").read
+  organisations_fixture_path = Rails.root.join("test/fixtures/worldwide/#{slug}_organisations.json")
+  File.open(organisations_fixture_path, "w") do |file|
+    file.puts json
+  end
 end
